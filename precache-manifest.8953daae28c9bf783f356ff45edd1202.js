d438276d5772a2c48b60e911490885e997e4e13a
sreehari vk <you@example.com>
2020-07-25 04:38:41 +0530

    Updates

---
+++ precache-manifest.8953daae28c9bf783f356ff45edd1202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81d03ea43c7a0604a0a67777d6ebe4f",
+    "url": "/sreeharivks/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a7cae3594e118e398045",
+    "url": "/sreeharivks/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "587b27c96c1eb64c746b",
+    "url": "/sreeharivks/covid-19-tracker/static/css/main.d8847db5.chunk.css"
+  },
+  {
+    "revision": "a7cae3594e118e398045",
+    "url": "/sreeharivks/covid-19-tracker/static/js/2.2ffecef9.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/sreeharivks/covid-19-tracker/static/js/2.2ffecef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587b27c96c1eb64c746b",
+    "url": "/sreeharivks/covid-19-tracker/static/js/main.b785002a.chunk.js"
+  },
+  {
+    "revision": "24b4934ba75f832ab623",
+    "url": "/sreeharivks/covid-19-tracker/static/js/runtime-main.70f74003.js"
+  }
+]);
